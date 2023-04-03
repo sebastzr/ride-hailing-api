0eb7465f8ac9bb172119553c4631fbad44ac1745
Sebastian Zapata <sebastzr@utp.edu.co>
2023-04-03 18:27:49 -0500

    Add payment service tests

---
+++ api/src/services/paymentSourceService.test.js
@@ -0,0 +1,43 @@
+const { createPaymentSource } = require("./paymentSourceService");
+const axios = require("axios");
+const PaymentSource = require("../models/PaymentSource");
+
+jest.mock("axios");
+jest.mock("../models/PaymentSource.js", () => {
+  return {
+    create: jest.fn(),
+  };
+});
+
+describe("createPaymentSource", () => {
+  it("should create a payment source", async () => {
+    const data = {
+      userID: "123",
+      token: "tok_123",
+      userEmail: "test@example.com",
+      type: "card",
+      acceptanceToken: "accept_123",
+    };
+
+    const response = {
+      data: {
+        data: {
+          id: "payment_source_id",
+        },
+      },
+    };
+
+    axios.post.mockResolvedValue(response);
+
+    const expectedPaymentSource = {
+      user: data.userID,
+      type: data.type,
+      paymentSourceId: response.data.data.id,
+    };
+    PaymentSource.create.mockReturnValue(expectedPaymentSource);
+
+    const paymentSource = await createPaymentSource(data);
+
+    expect(paymentSource).toEqual(expectedPaymentSource);
+  });
+});
--- readme.md
+++ readme.md
@@ -6,3 +6,18 @@
 2. Go to the root of the project and run `cp api/.env.example api/.env`
    - Here you need to provide the WOMPI PRIVATE KEY
 3. At the root of the project run `docker-compose up --build`
+
+## Try it
+
+Using the postman collection:
+
+1. Create an Acceptance Token
+2. Tokenize a dummy Card
+3. When you run the docker-compose, the seeds generate some rider ids, copy one.
+4. Get the data from steps 1 - 2 - 3 and paste them into the Payment Sources request to create a new payment method.
+5. With the payment method created you can start to Request Rides and Finish Rides.
+
+## Run tests
+
+1. `cd api`
+2. `npm test`
